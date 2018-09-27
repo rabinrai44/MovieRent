51408bae1d7e60f702051852e34ab770436b8f5d
Rabin Rai <rabinrai44@gmail.com>
2018-09-27 00:53:50 -0400

    Added customerscontroller, moviescontroller, display customer list the display individual details view with associate id

---
+++ MovieRent/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using MovieRent.Models;
+using MovieRent.ViewModels;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MovieRent.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+           
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek!" },
+                new Movie {Id = 2, Name = "The Crazy Man" },
+                new Movie {Id = 3, Name = "The Final Destination" }
+            };
+        }
+
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
