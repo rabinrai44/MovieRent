647e9e0e024bda85ea96f4f21e152f4f15c24813
Rabin Rai <rabinrai44@gmail.com>
2018-09-27 15:15:33 -0400

    Added quering objects rather than using hardcoded list

--- MovieRent/Controllers/CustomersController.cs
+++ MovieRent/Controllers/CustomersController.cs
@@ -1,5 +1,5 @@
 ﻿using MovieRent.Models;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -7,31 +7,33 @@ namespace MovieRent.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customer
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)            
                 return HttpNotFound();
             return View(customer);
             
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer> {
-                new Customer { Id = 1, Name = "Rabin" },
-                new Customer { Id = 2, Name = "Eunice" },
-                new Customer { Id = 3, Name = "Aron" },
-                new Customer { Id = 4, Name = "Ren" }
-            };
         }
     }
 }
