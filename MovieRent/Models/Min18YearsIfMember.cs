80246b0585674d50c6fef2fdd6227b354667707b
Rabin Rai <rabinrai44@gmail.com>
2018-09-28 17:53:09 -0400

    Added advanced validation functionality for creating new customer

---
+++ MovieRent/Models/Min18YearsIfMember.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieRent.Models
+{
+    public class Min18YearsIfMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (customer.BirthDate == null)
+                return new ValidationResult("Birth is required.");
+            var age = DateTime.Today.Year - customer.BirthDate.Value.Year;
+
+            return (age >= 18)
+                ? ValidationResult.Success
+                : new ValidationResult("Customer should be at least 18 years old to go a membership");
+        }
+    }
+}
