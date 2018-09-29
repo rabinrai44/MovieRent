843605af48d194de40d112adda5fcf232740b523
Rabin Rai <rabinrai44@gmail.com>
2018-09-29 01:48:50 -0400

    Added Dtos(Data transfer objects)

---
+++ MovieRent/Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using MovieRent.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieRent.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
