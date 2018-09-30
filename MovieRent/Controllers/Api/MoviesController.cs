63cbf584687287f394055eca18529ba293c07899
Rabin Rai <rabinrai44@gmail.com>
2018-09-30 01:56:33 -0400

    Added Movie API

---
+++ MovieRent/Controllers/Api/MoviesController.cs
@@ -0,0 +1,77 @@
+﻿using MovieRent.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace MovieRent.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: /api/movies
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movies.ToList();
+        }
+
+        // GET: /api/movies/4
+        public Movie GetMovie(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return movie;
+        }
+
+        // POST: /api/movies
+        [HttpPost]
+        public Movie Create(Movie movie)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return movie;
+        }
+
+        // PUT: /api/movies
+        [HttpPut]
+        public void Update(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+                       
+            _context.SaveChanges();
+
+        }
+
+        // DELETE: /api/movies/4
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+        }
+    }
+}
