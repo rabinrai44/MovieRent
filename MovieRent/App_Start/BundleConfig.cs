91e3629be05874622df749f5b693e40c48395dc6
Rabin Rai <rabinrai44@gmail.com>
2018-10-06 01:25:53 -0400

    Added bootboxjs Plug-in for nicer delete cofirm dialog model

--- MovieRent/App_Start/BundleConfig.cs
+++ MovieRent/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace MovieRent
 {
@@ -20,7 +19,8 @@ namespace MovieRent
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
