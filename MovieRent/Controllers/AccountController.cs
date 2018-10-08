c5b7fade98faa07f3f7f0e87f22f83c35fc40048
Rabin Rai <rabinrai44@gmail.com>
2018-10-08 01:36:08 -0400

    Added Facebook login authentication, Enabled the SSL

--- MovieRent/Controllers/AccountController.cs
+++ MovieRent/Controllers/AccountController.cs
@@ -368,7 +368,11 @@ namespace MovieRent.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    DrivingLicense = model.DrivingLicense
+                };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
