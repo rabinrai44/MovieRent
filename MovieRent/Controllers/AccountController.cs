84bddbde625e46261ab85840583f4243dffbdbf7
Rabin Rai <rabinrai44@gmail.com>
2018-10-08 16:35:46 -0400

    Added Phone to Profile Data

--- MovieRent/Controllers/AccountController.cs
+++ MovieRent/Controllers/AccountController.cs
@@ -151,7 +151,8 @@ namespace MovieRent.Controllers
                 var user = new ApplicationUser {
                     UserName = model.Email,
                     Email = model.Email,
-                    DrivingLicense = model.DrivingLicense
+                    DrivingLicense = model.DrivingLicense,
+                    Phone = model.Phone
                 };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
