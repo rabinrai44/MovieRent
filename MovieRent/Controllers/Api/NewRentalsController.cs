9ff9fab18d247cb358f906088d7c54169d1e5eba
Rabin Rai <rabinrai44@gmail.com>
2018-10-09 00:11:04 -0400

    Added New feature as a NewRentals

---
+++ MovieRent/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,15 @@
+﻿using MovieRent.Dtos;
+using System;
+using System.Web.Http;
+
+namespace MovieRent.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MovieRent/Dtos/NewRentalDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MovieRent.Dtos
+{
+    public class NewRentalDto
+    {
+        public int CustomerId { get; set; }
+
+        public List<int> MoviIds { get; set; }
+    }
+}
